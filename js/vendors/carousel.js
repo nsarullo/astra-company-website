2ea83707b983aae773898042a87dd0a96074f737
nsarullo <51282484+nsarullo@users.noreply.github.com>
2024-01-05 13:06:13 -0800

    our

--- js/vendors/carousel.js
+++ js/vendors/carousel.js
@@ -11,23 +11,3 @@ $('.carousel').slick({
     touchMove: true,
     fade: true,
   });
-
-  // @media only screen and (max-width: 767px) {
-  //   .carousel {
-  //     display: none;
-  //   }
-  // }
-
-  // function updateCarousel() {
-  //   if(window.innerWidth < 767) {
-  //     console.log($('.carousel').slick('slickGetOption', 'fade'));
-  //     $('.carousel').slick('slickSetOption', 'fade', true, true);
-  //     console.log($('.carousel').slick('slickGetOption', 'fade'));
-  //     // $('.carousel').slick('slickSetOption', 'swipe', false, true);
-  //     // $('.carousel').slick('slickSetOption', 'touchMove', false, true);
-  //   }
-  // }
-
-  // window.addEventListener('resize', updateCarousel);
-
-  // updateCarousel();
